5cc38520534be215892b0031d25d7aa247021664
sitaladhikari002@gmail.com <sitaladhikari002@gmail.com>
2023-06-14 17:43:20 +0545

    update:create blog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-
-
-
 import Login from "./Login/Login"
-
+import { Route, Routes } from "react-router-dom"
+import SignUp from "./pages/SignUp/sign-up"
+import Home from "./pages/Home/home"
+import NewBlog from "./pages/New Blog/new-blog"
 const App = () => {
   return (
     <div>
-      <Login/>
+      <Routes>
+        <Route path="/" index element={<Home/>}/>
+        <Route path="/blog/new" element={<NewBlog/>}/>
+        <Route path="/signin" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="*" element={<h1>404 Error</h1>}/>
+      </Routes>
     </div>
 )
     }
---
+++ src/pages/New Blog/new-blog.tsx
@@ -0,0 +1,40 @@
+import {useState,useEffect,useRef } from "react"
+import SunEditor from 'suneditor-react';
+import SunEditorCore from "suneditor/src/lib/core";
+import 'suneditor/dist/css/suneditor.min.css';;
+
+const NewBlog = () => {
+    const [value, setValue] = useState<string>('');
+    const previewRef = useRef<HTMLDivElement>(null)
+    const editor = useRef<SunEditorCore>();
+    useEffect(() => {
+        // console.log(value)
+        
+    }, [value])
+    const handleContentChange = (e:any)=>{
+        // console.log(e);
+        setValue(e);
+        if(previewRef.current){
+            previewRef.current.innerHTML = e;
+        }
+    }
+    if(editor.current){
+        editor.current.onChange = (e)=>{
+            console.log(e)
+            console.log(editor.current) 
+        }
+    }
+    
+      return  <>
+      <SunEditor
+      onChange={handleContentChange}
+      setContents="Initial content"
+
+    />
+    <h1>Preview</h1>
+    <div ref={previewRef} ></div>
+      </> 
+
+}
+
+export default NewBlog
